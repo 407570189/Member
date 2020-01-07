1b924c31fafb85671f2990025b32d5ad0317e146
miia <miia@com.tw>
2020-01-07 15:31:42 +0800

    2nd

--- app/src/main/java/com/fju/member/MainActivity.java
+++ app/src/main/java/com/fju/member/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView name;
     private TextView age;
     private TextView gender;
-    public boolean logon=false;
+    public boolean logon = false;
     private String Name;
     private String Age;
     private String Gender;
@@ -23,26 +23,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SharedPreferences sp=getSharedPreferences("test",MODE_PRIVATE);
+        SharedPreferences sp = getSharedPreferences("test", MODE_PRIVATE);
         name = findViewById(R.id.name1);
         age = findViewById(R.id.age1);
         gender = findViewById(R.id.gender1);
-        name.setText(sp.getString("NAME",""));
-        age.setText(sp.getString("AGE",""));
-        gender.setText(sp.getString("GENDER",""));
+        name.setText(sp.getString("NAME", ""));
+        age.setText(sp.getString("AGE", ""));
+        gender.setText(sp.getString("GENDER", ""));
         Name = name.getText().toString();
         Age = age.getText().toString();
         Gender = gender.getText().toString();
-        if(!TextUtils.isEmpty(Name) && !TextUtils.isEmpty(Age) && !TextUtils.isEmpty(Gender)) {
-            new AlertDialog.Builder(MainActivity.this)
-                    .setTitle("成功訊息")
-                    .setMessage("成功申請帳號")
-                    .setPositiveButton("OK",null)
-                    .show();
-        }else{
-            Intent intent = new Intent(MainActivity.this,NicknameActivity.class);
-            startActivityForResult(intent,RT);
+        if (!logon) {
+            Intent intent = new Intent(this, NicknameActivity.class);
+            startActivity(intent);
         }
+
 
     }
 }

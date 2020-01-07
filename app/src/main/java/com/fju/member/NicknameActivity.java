240fa8537396c238d6a72f14b1c23ab11d41bdd1
miia <miia@com.tw>
2020-01-07 13:57:56 +0800

    1

--- app/src/main/java/com/fju/member/NicknameActivity.java
+++ app/src/main/java/com/fju/member/NicknameActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class NicknameActivity extends AppCompatActivity {
-
+    String a="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
